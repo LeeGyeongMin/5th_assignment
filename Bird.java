9fce20bb95b80c60ff4109c288da2265faa4ba5e
LeeGyeongMin <31638900+LeeGyeongMin@users.noreply.github.com>
2017-09-14 15:18:03 +0900

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,12 @@
+
+public class Bird extends Animal{
+	public Bird(){
+		System.out.println("A new bird has been created!");
+	}
+	public void sleep(){
+		System.out.println("An bird sleeps. . . ");
+	}
+	public void eat(){
+		System.out.println("An bird eats. . . ");
+	}
+}
